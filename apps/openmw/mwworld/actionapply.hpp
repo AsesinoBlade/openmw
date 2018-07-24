7d5f3c74458f35eb757cbe160aaf68fe4060a235
Andrei Kortunov <andrei.kortunov@yandex.ru>
2018-07-24 12:31:31 +0400

    add support for poisons
    If a potion is a poison and you consume it, it will be added to your equipped weapon.  If you have no equipped weapon, you will drink it instead.
    
    changed from one-hit poison to random chance removal of poison based on alchemy skill and luck; if first random roll > alchemy skill and second random roll > luck, you lose the poison
    added messages to inform of poison statuses

--- apps/openmw/mwworld/actionapply.hpp
+++ apps/openmw/mwworld/actionapply.hpp
@@ -16,6 +16,16 @@ namespace MWWorld
     public:
         ActionApply(const Ptr& object, const ESM::RefId& id);
     };
+
+    class ActionPoison : public Action
+    {
+        ESM::RefId mId;
+
+        virtual void executeImp(const Ptr& actor);
+
+    public:
+        ActionPoison(const Ptr& object, const ESM::RefId id);
+    };
 }
 
 #endif
