26d98a8c02c6d61dd91dc5b6f258dd9899a18780
George.Saeed <gsaeed@yahoo.com>
2021-09-08 17:26:44 +0100

    added index number to log so you can see which mod is in which index position

--- apps/openmw/mwworld/contentloader.hpp
+++ apps/openmw/mwworld/contentloader.hpp
@@ -1,22 +1,25 @@
 #ifndef CONTENTLOADER_HPP
 #define CONTENTLOADER_HPP
 
-#include <filesystem>
 
-namespace Loading
-{
-    class Listener;
-}
+#include <filesystem>
+#include <MyGUI_TextIterator.h>
+
+
+#include <components/debug/debuglog.hpp>
+#include "components/loadinglistener/loadinglistener.hpp"
 
 namespace MWWorld
 {
+
 
     struct ContentLoader
     {
         virtual ~ContentLoader() = default;
 
         virtual void load(const std::filesystem::path& filepath, int& index, Loading::Listener* listener) = 0;
-    };
+};
+
 
 } /* namespace MWWorld */
 
