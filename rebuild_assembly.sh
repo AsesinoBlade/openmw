257ce46c798f1b2256dc7b4391b7d7369e9742ee
George.Saeed <gsaeed@yahoo.com>
2021-09-23 15:23:41 +0100

    created shell script to rebuild assemblies

---
+++ rebuild_assembly.sh
@@ -0,0 +1,8 @@
+cd C:/Users/gsaee/Projects/openmw
+rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2019_64/Release
+rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2019_64/RelWithDebInfo
+rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2019_64/Debug
+rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2019_64/MinSizeRel
+cd C:/Users/gsaee/Projects/openmw
+./CI/before_script.msvc.sh -k -p Win64 -v 2019
+
