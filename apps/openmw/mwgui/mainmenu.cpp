0e07b1c3575d17771f5c6ab5192f1b19732c841b
George.Saeed <gsaeed@yahoo.com>
2021-09-23 23:10:37 +0100

    added autosave on exit

--- apps/openmw/mwgui/mainmenu.cpp
+++ apps/openmw/mwgui/mainmenu.cpp
@@ -153,6 +153,7 @@ namespace MWGui
 
     void MainMenu::onExitConfirmed()
     {
+        MWBase::Environment::get().getStateManager()->quickSave("Autosave"); 
         MWBase::Environment::get().getStateManager()->requestQuit();
     }
 
