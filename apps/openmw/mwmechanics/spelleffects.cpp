da09cea062399347fe91e7bc207ea0e5673bdc44
George.Saeed <gsaeed@yahoo.com>
2021-10-02 22:39:10 +0100

    defaulted actionTeleport to true for include companions

--- apps/openmw/mwmechanics/spelleffects.cpp
+++ apps/openmw/mwmechanics/spelleffects.cpp
@@ -491,7 +491,7 @@ namespace MWMechanics
                     if (markedCell)
                     {
                         ESM::RefId dest = markedCell->getCell()->getId();
-                        MWWorld::ActionTeleport action(dest, markedPosition, false);
+                        MWWorld::ActionTeleport action(dest, markedPosition, true);
                         action.execute(target);
                         if (!caster.isEmpty())
                         {
