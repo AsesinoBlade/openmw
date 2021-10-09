7341d327df0ba29572062af65748629fe24118fc
George.Saeed <gsaeed@yahoo.com>
2021-10-09 16:31:52 +0100

    added console command GetGlobal to show global variables
    You can still run showvars to show all variables but now you can also run getglobal globalVariableName to show the value
    for example
    getglobal timescale
    or
    gg timescale
    to see the value of timescale
    
    setting is still set so changing timescale is
    set timescale to 8

--- components/compiler/extensions0.cpp
+++ components/compiler/extensions0.cpp
@@ -330,6 +330,8 @@ namespace Compiler
             extensions.registerInstruction("t3d", "", opcodeTestModels);
             extensions.registerInstruction("raq", "", opcodeReportActiveQuests);
             extensions.registerInstruction("reportactivequests", "", opcodeReportActiveQuests);
+            extensions.registerInstruction("getglobal", "c", opcodeGetGlobal);
+            extensions.registerInstruction("gg", "c", opcodeGetGlobal);
         }
     }
 
