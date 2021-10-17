1381255fde31e2b4e7050e54208e1aa1fe7100a8
George.Saeed <gsaeed@yahoo.com>
2021-10-17 19:39:18 +0100

    add metadata to filter so you can filter on weapon armor light armor, etc.

--- apps/openmw/mwclass/creature.cpp
+++ apps/openmw/mwclass/creature.cpp
@@ -217,6 +217,11 @@ namespace MWClass
         return getNameOrId<ESM::Creature>(ptr);
     }
 
+    std::string_view Creature::getSearchTags(const MWWorld::ConstPtr& ptr) const
+    {
+        return " creature ";
+    }
+
     MWMechanics::CreatureStats& Creature::getCreatureStats(const MWWorld::Ptr& ptr) const
     {
         ensureCustomData(ptr);
