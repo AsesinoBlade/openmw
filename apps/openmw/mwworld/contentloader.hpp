a75d4f0026919abd80a866c3cef2297a1c43920b
George.Saeed <gsaeed@yahoo.com>
2021-12-09 21:36:58 +0000

    refactored for source openmw changes

--- apps/openmw/mwworld/contentloader.hpp
+++ apps/openmw/mwworld/contentloader.hpp
@@ -1,7 +1,6 @@
 #ifndef CONTENTLOADER_HPP
 #define CONTENTLOADER_HPP
 
-
 #include <filesystem>
 #include <MyGUI_TextIterator.h>
 
@@ -24,3 +23,4 @@ namespace MWWorld
 } /* namespace MWWorld */
 
 #endif /* CONTENTLOADER_HPP */
+
