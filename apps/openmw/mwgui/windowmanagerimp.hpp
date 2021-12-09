bab2f048505e64deba403bfa5086bfa1edab398b
George.Saeed <gsaeed@yahoo.com>
2021-12-09 21:36:58 +0000

    refactored for source openmw changes

--- apps/openmw/mwgui/windowmanagerimp.hpp
+++ apps/openmw/mwgui/windowmanagerimp.hpp
@@ -196,7 +196,6 @@ namespace MWGui
         MWGui::CompanionWindow* getCompanionWindow() override;
         void setCompanionWindow(MWGui::CompanionWindow* containerWindow) override;
 
-
         /// Make the player use an item, while updating GUI state accordingly
         void useItem(const MWWorld::Ptr& item, bool bypassBeastRestrictions = false) override;
 
@@ -463,7 +462,6 @@ namespace MWGui
         CountDialog* mCountDialog;
         TradeWindow* mTradeWindow;
         ItemModel* mItemModel = nullptr;
-        ContainerWindow* mContainerWindow = nullptr;
         CompanionWindow* mCompanionWindow = nullptr;
         SettingsWindow* mSettingsWindow;
         ConfirmationDialog* mConfirmationDialog;
@@ -481,6 +479,7 @@ namespace MWGui
         DebugWindow* mDebugWindow;
         PostProcessorHud* mPostProcessorHud;
         JailScreen* mJailScreen;
+        ContainerWindow* mContainerWindow;
 
         ControllerButtonsOverlay* mControllerButtonsOverlay;
         InventoryTabsOverlay* mInventoryTabsOverlay;
