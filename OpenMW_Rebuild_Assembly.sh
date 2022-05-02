d2ace715bb67ed3c8bca270a591b1ffe7647a39d
George.Saeed <gsaeed@yahoo.com>
2022-05-02 20:19:46 +0100

    added line to delete file CMakeCache.txt

--- OpenMW_Rebuild_Assembly.sh
+++ OpenMW_Rebuild_Assembly.sh
@@ -4,5 +4,6 @@ rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2019_64/RelWithDebInfo
 rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2019_64/Debug
 rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2019_64/MinSizeRel
 cd C:/Users/gsaee/Projects/openmw
+rm CMakeCache.txt
 ./CI/before_script.msvc.sh -k -p Win64 -v 2019
 
