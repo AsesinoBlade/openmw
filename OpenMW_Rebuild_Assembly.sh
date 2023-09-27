903bd0f6f32d0c7168d60584922412ed5e3cc2c5
George.Saeed <gsaeed@yahoo.com>
2023-09-27 12:42:37 +0100

    Changed rebuild script to delete entire MSVC2022_64 folder

--- OpenMW_Rebuild_Assembly.sh
+++ OpenMW_Rebuild_Assembly.sh
@@ -1,8 +1,6 @@
 cd C:/Users/gsaee/Projects/openmw
-rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2022_64/Release
-rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2022_64/RelWithDebInfo
-rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2022_64/Debug
-rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2022_64/MinSizeRel
+rm -rfv C:/Users/gsaee/Projects/openmw/MSVC2022_64
+
 cd C:/Users/gsaee/Projects/openmw
 rm CMakeCache.txt
 ./CI/before_script.msvc.sh -k -p Win64 -v 2022
