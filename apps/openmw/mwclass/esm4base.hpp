11ff2c013557eb099e0926d55128b93fef58da72
George.Saeed <gsaeed@yahoo.com>
2023-09-30 12:29:51 +0100

    added getSearchTags to esm4base so it can be available to esm4 classes.

--- apps/openmw/mwclass/esm4base.hpp
+++ apps/openmw/mwclass/esm4base.hpp
@@ -106,7 +106,7 @@ namespace MWClass
 
         std::string_view getName(const MWWorld::ConstPtr& ptr) const override { return {}; }
 
-        std::string_view getSearchTags(const MWWorld::ConstPtr& ptr) const override { return ""; }
+        std::string_view getSearchTags(const MWWorld::ConstPtr& ptr) const override { return {}; }
 
         std::string_view getModel(const MWWorld::ConstPtr& ptr) const override
         {
