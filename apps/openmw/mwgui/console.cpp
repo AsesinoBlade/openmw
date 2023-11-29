92db38935978908c55f5edbb5603678d7aa4b8eb
George.Saeed <gsaeed@yahoo.com>
2023-11-29 13:24:47 +0000

    repositioning requires shift and ctrl keys pressed together.  Shift alone just does vanilla logic

--- apps/openmw/mwgui/console.cpp
+++ apps/openmw/mwgui/console.cpp
@@ -350,7 +350,8 @@ namespace MWGui
 
         auto obj = ref.getRefData();
 
-        if (MyGUI::InputManager::getInstance().isShiftPressed())
+        if (MyGUI::InputManager::getInstance().isShiftPressed()
+            && MyGUI::InputManager::getInstance().isControlPressed())
         {
             ESM::Position position = obj.getPosition();
             float scale = cellRef.getScale();
@@ -602,6 +603,7 @@ namespace MWGui
 
         else if (!mPtr.isEmpty() 
             && MyGUI::InputManager::getInstance().isShiftPressed()
+            && MyGUI::InputManager::getInstance().isControlPressed()
             && key != MyGUI::KeyCode::LeftShift 
             && key != MyGUI::KeyCode::RightShift
             )
