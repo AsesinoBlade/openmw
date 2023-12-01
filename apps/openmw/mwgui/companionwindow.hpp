f61ac2358c22981f87933a76a226220e5a3c5eac
George.Saeed <gsaeed@yahoo.com>
2023-12-01 13:05:56 +0000

    add sorting to inventory items (by name, -name, type, -type, value, -value, weight, -weight, value by weight, and -value by weight
    Note the categories that start with - are descending sorts

--- apps/openmw/mwgui/companionwindow.hpp
+++ apps/openmw/mwgui/companionwindow.hpp
@@ -46,7 +46,6 @@ namespace MWGui
         void setActiveControllerWindow(bool active) override;
 
         MWGui::ItemView* getItemView() { return mItemView; }
-        CompanionItemModel* getModel() { return mModel; }
 
     private:
         ItemView* mItemView;
