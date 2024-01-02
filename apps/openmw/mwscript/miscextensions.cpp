0da96d3501bd3defa313003df7deaeaf0d83af6e
George.Saeed <gsaeed@yahoo.com>
2024-01-02 15:57:12 +0000

    getCount() was moved from getRef to getCellRef in commit 'use NAM9 for stack count'

--- apps/openmw/mwscript/miscextensions.cpp
+++ apps/openmw/mwscript/miscextensions.cpp
@@ -1787,6 +1787,7 @@ class OpSetFollowers : public Interpreter::Opcode0
 
                 if (ptr.getRefData().isDeletedByContentFile())
                     msg << "[Deleted by content file]" << std::endl;
+
                 auto &refD = ptr.getRefData();
                 if (!refD.getBaseNode())
                     msg << "[Deleted]" << std::endl;
