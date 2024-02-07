7477aed5cd025152d332d7cf94a3ec69f6bb7f9e
George.Saeed <gsaeed@yahoo.com>
2024-02-07 12:45:51 +0000

    recast getModel from string_view to string

--- apps/openmw/mwscript/miscextensions.cpp
+++ apps/openmw/mwscript/miscextensions.cpp
@@ -1769,8 +1769,7 @@ class OpSetFollowers : public Interpreter::Opcode0
                 if (ptr.getRefData().isDeletedByContentFile())
                     msg << "[Deleted by content file]" << std::endl;
 
-                auto &refD = ptr.getRefData();
-                if (!refD.getBaseNode())
+                if (!ptr.getCellRef().getCount())
                     msg << "[Deleted]" << std::endl;
 
                 msg << "Name: [" << ptr.getClass().getName(ptr) << "] " << std::endl;
@@ -1799,6 +1798,7 @@ class OpSetFollowers : public Interpreter::Opcode0
 
                 auto vfs = MWBase::Environment::get().getResourceSystem()->getVFS();
                 std::string model = ::Misc::ResourceHelpers::correctActorModelPath(std::string(ptr.getClass().getModel(ptr)), vfs);
+
                 msg << "Model: " << model << std::endl;
                 if (!model.empty())
                 {
